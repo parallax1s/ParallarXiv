621542312cf80be3ef037e024fdb9e54e3ca8fb5
parallax1s <peilosunited@gmail.com>
2025-05-21 02:42:08 +0200

    Add paper generator API

--- pages/index.js
+++ pages/index.js
@@ -9,6 +9,7 @@ export default function Home() {
       <main>
         <h1>ParallarXiv</h1>
         <p>Deployment successful!</p>
+        <p>Visit <code>/api/papers</code> for generated paper data.</p>
       </main>
     </>
   )
